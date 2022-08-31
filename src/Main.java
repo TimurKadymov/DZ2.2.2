6c4d916b4a7ed8db3677d8ab7b2e4eb1ef60ab47
Тимур Кадымов <tkadymov95@gmail.com>
2022-08-31 13:37:15 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int i = 300;
-        int p = 3000;
-        int x = i+p;
+        int popolnenya = 1000;
+        int balans = i + popolnenya;
         int percent;
-        if (p > 1000) {
+        if (popolnenya >= 1000) {
             percent = 1;
         } else {
             percent = 0;
         }
         //int percent = p >= 1000 ? 1 : 0;
-        int bonus = p * percent / 100;
+        int bonus = popolnenya * percent / 100;
+        System.out.println("Итоговый бонус: " + bonus);
     }
 }
