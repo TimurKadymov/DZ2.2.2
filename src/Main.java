b77e8560767e9a736b1031992aebf62660987fb8
Тимур Кадымов <tkadymov95@gmail.com>
2022-09-05 23:30:14 +0500

    Присвоил названия всем переменным

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int i = 300;
-        int popolnenya = 1000;
-        int balans = i + popolnenya;
+        int balance = 300;
+        int deposit_amount = 1000;
+        int balance_final = balance + deposit_amount;
         int percent;
-        if (popolnenya >= 1000) {
+        if (deposit_amount >= 1000) {
             percent = 1;
         } else {
             percent = 0;
         }
         //int percent = p >= 1000 ? 1 : 0;
-        int bonus = popolnenya * percent / 100;
+        int bonus = deposit_amount * percent / 100;
         System.out.println("Итоговый бонус: " + bonus);
     }
 }
